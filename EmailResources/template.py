d31cc6a6021bf5127f19e985879a5224d61b930f
Ashutosh Biswal <77984053+ashdroid4@users.noreply.github.com>
2024-07-18 14:51:27 +0530

    Update template.py

--- EmailResources/template.py
+++ EmailResources/template.py
@@ -1,3 +1,7 @@
+"""This is the template file where you add your html and body"""
+
+# An example has been done for you!
+
 html = '''
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="und" style="font-family:'Exo 2', sans-serif">
